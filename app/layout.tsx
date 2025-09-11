35783fcf3f235c47c9e431c063e4fcdd340889a5
Love Aggarwal <loveaggarwalcta@gmail.com>
2025-09-11 23:48:48 +0530

    landing page

---
+++ app/layout.tsx
@@ -0,0 +1,42 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import { ClerkProvider } from '@clerk/nextjs';
+
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Sportwave",
+  description: "An all-in-one sports app",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+         <ClerkProvider
+            appearance={{
+                cssLayerName: 'clerk',
+            }}
+        >
+            {children}
+        </ClerkProvider>
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+
+
+import Header from "@/components/header";
+import Hero from "@/components/homepage";
+export default function Home() {
+  return (
+    <div>
+      <Header />
+      <Hero />
+    </div>
+  );
+}
